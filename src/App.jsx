5f671b8471b4f619c43aecf0678c937a437042bd
Zoran Simonovic <simonovic1980@gmail.com>
2023-05-31 12:46:52 +0200

    react router & nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
+import { Link } from "react-router-dom";
+// , Route, Switch
+
 function App() {
   return (
-    <>
-      <div className="text-center">Hello!</div>
-    </>
+    <div className="bg-blue-200">
+      <nav className="bg-amber-100 flex space-x-4">
+        <ul className="flex space-x-4">
+          <li className="p-4 hover:bg-amber-300 font-semibold">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="p-4 hover:bg-amber-300 font-semibold">
+            <Link to="/useReducerA">useReducer 1</Link>
+          </li>
+          <li className="p-4 hover:bg-amber-300 font-semibold">
+            <Link to="/useReducerB">useReducer 2</Link>
+          </li>
+          <li className="p-4 hover:bg-amber-300 font-semibold">
+            <Link to="/useContextA">useContext</Link>
+          </li>
+          <li className="p-4 hover:bg-amber-300 font-semibold">
+            <Link to="/useContextA">useMemo</Link>
+          </li>
+          <li className="p-4 hover:bg-amber-300 font-semibold">
+            <Link to="/useContextA">useCallback</Link>
+          </li>
+          <li className="p-4 hover:bg-amber-300 font-semibold">
+            <Link to="/useContextA">useRef</Link>
+          </li>
+          {/* <li className="p-4 hover:bg-amber-300 font-semibold">
+            <Link to="/contact">Contact</Link>
+          </li> */}
+        </ul>
+      </nav>
+    </div>
   );
 }
 
